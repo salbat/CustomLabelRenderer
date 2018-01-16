c935f14856aff9bfb8c031cd09fde6a15f03d863
salbat <0xack13@gmail.com>
2018-01-17 00:14:59 +1100

    Add project files.

---
+++ LargeTextLabel/LargeTextLabel.Android/CustomLabelRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Android.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace LargeTextLabel.Droid
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetMaxLines(int.MaxValue);
+            }
+        }
+    }
+}
